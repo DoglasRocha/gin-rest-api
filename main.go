8ef8f0f3df9c8886f3aaf6cff7b727306eecc656
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-07-08 22:32:32 -0300

    create students route

--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"gin-rest-api/database"
-	"gin-rest-api/models"
 	"gin-rest-api/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Doglas", CPF: "00000000000", RG: "000000000"},
-		{Nome: "Ana", CPF: "11111111111", RG: "111111111"},
-	}
 	routes.HandleRequests()
 }
