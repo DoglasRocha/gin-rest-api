27a689ed019644a0cc2907261b2703d646ac73fc
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-07-08 22:37:59 -0300

    create student search by id route

--- controllers/controller.go
+++ controllers/controller.go
@@ -33,3 +33,10 @@ func CriaNovoAluno(c *gin.Context) {
 	database.DB.Create(&aluno)
 	c.JSON(http.StatusOK, aluno)
 }
+
+func BuscaAlunoPorId(c *gin.Context) {
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	database.DB.First(&aluno, id)
+	c.JSON(http.StatusOK, aluno)
+}
