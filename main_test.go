29bd83db83520801b7fae0aab121e4cbea2d0f99
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-07-10 21:42:09 -0300

    test 'alunos' endpoint

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gin-rest-api/controllers"
+	"gin-rest-api/database"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -11,13 +12,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func SetupDasRotasDeTest() *gin.Engine {
+func SetupDasRotasDeTeste() *gin.Engine {
 	rotas := gin.Default()
 	return rotas
 }
 
 func TestVerificaStatusCodeDaSaudacao(t *testing.T) {
-	r := SetupDasRotasDeTest()
+	r := SetupDasRotasDeTeste()
 	r.GET("/:nome", controllers.Saudacao)
 	req, _ := http.NewRequest("GET", "/doglas", nil)
 	resp := httptest.NewRecorder()
@@ -27,4 +28,15 @@ func TestVerificaStatusCodeDaSaudacao(t *testing.T) {
 	mockDaResp := `{"API diz:":"E ai doglas, tudo bem?"}`
 	respBody, _ := ioutil.ReadAll(resp.Body)
 	assert.Equal(t, mockDaResp, string(respBody))
+}
+
+func TestListandoTodosOsAlunosHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+
+	assert.Equal(t, http.StatusOK, resp.Code)
 }
