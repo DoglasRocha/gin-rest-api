4510cdccfa14bb0224cc817ee24f88501f865f2a
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-07-10 22:53:50 -0300

    render HTML with Gin

--- controllers/controller.go
+++ controllers/controller.go
@@ -94,7 +94,10 @@ func BuscaAlunoPorCPF(c *gin.Context) {
 }
 
 func ExibePaginaIndex(c *gin.Context) {
+	var alunos []models.Aluno
+	database.DB.Find(&alunos)
+
 	c.HTML(http.StatusOK, "index.html", gin.H{
-		"mensagem": "Boas vindas",
+		"alunos": alunos,
 	})
 }
